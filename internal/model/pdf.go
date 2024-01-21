94e3c0d6517633898f4388af4aea0eb640065b16
alysson <34694287+nu12@users.noreply.github.com>
2024-01-21 17:20:30 -0500

    Initial commit

---
+++ internal/model/pdf.go
@@ -0,0 +1,7 @@
+/*
+Copyright © 2024 nu12
+*/
+
+package model
+
+type PDF struct{}
---
+++ main.go
@@ -0,0 +1,10 @@
+/*
+Copyright © 2024 nu12
+*/
+package main
+
+import "github.com/nu12/pdf/cmd"
+
+func main() {
+	cmd.Execute()
+}
