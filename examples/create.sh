a786ef331b1cfb400802f9d135e33a8299da20f5
nu12 <34694287+nu12@users.noreply.github.com>
2024-02-02 20:51:53 -0500

    Change text

--- examples/create.sh
+++ examples/create.sh
@@ -6,7 +6,7 @@ for FORMAT in $(ls examples/inputs/); do
     for COMPRESSION in NONE LOWEST LOW MEDIUM HIGH HIGHEST; do
         for MARGIN in NONE MIN MEDIUM MAX; do
             for MODE in P L; do
-                echo "Creating document: $FORMAT, Compression $COMPRESSION, Margin $MARGIN, Mode $MODE"
+                echo "Creating document: $FORMAT, Compression $COMPRESSION, Margin $MARGIN, Orientation $MODE"
                 ./pdfTest create examples/inputs/$FORMAT/*.png examples/outputs/$FORMAT$MODE-$COMPRESSION-$MARGIN.pdf -c $COMPRESSION -m $MARGIN -o $MODE
             done
         done    
