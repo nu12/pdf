8f2ecf09b81d21bd94253fd93b13dcb2ecb47644
nu12 <34694287+nu12@users.noreply.github.com>
2025-05-14 22:40:09 -0400

    Improve verison output and texts (#5)

--- cmd/append.go
+++ cmd/append.go
@@ -14,7 +14,18 @@ func init() {}
 var appendCmd = &cobra.Command{
 	Use:   "append input1 [input2 input3 ... inputN] output",
 	Short: "Append PDFs and images into a single PDF file",
-	Long:  `Append PDFs and images into a single PDF file.`,
+	Long: `Append PDFs and images into a single PDF file
+	
+Examples:
+
+# Append two PDFs into a single PDF file:
+pdf append input1.pdf input2.pdf output.pdf
+
+# Append PDFs and images into a single PDF file:
+pdf append input1.pdf image1.jpg output.pdf
+
+# Append all files from a folder into a single PDF file:
+pdf append path-to-files/* output.pdf`,
 
 	Run: func(cmd *cobra.Command, args []string) {
 		pdf.InitAppend(args, tmpDir).PreProcess().Run().PostProcess()
